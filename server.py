7282fa69220b9acbbfabf0a08ecc84180f39cd4d
rodrigcasio <rod.casio@outlook.com>
2025-12-05 17:31:33 -0600

    `feat:` Add new practice exercise to deploy

--- server.py
+++ server.py
@@ -8,23 +8,41 @@ app = Flask("Mathematics Problem Solver")
 def sum_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here
+
+    result = summation(num1, num2)
+    if result.is_integer():
+        result = int(result)
+        return str(result)
+    else:
+        return ({ "message": "Not a whole number"}, 403)
 
 @app.route("/sub")
 def sub_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here
+
+    result = subtraction(num1, num2)
+    if result.is_integer():
+        result = int(result)
+        return str(result)
+    else:
+        return ({ "message": "Not a whole number"}, 403)
 
 @app.route("/mul")
 def mul_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here  
+
+    result = multiplication(num1, num2)
+    if result.is_integer():
+        result = int(result)
+        return str(result)
+    else:
+        return ({ "message": "Not a whole number"}, 403)
 
 @app.route("/")
 def render_index_page():
-    # Write your code here
+     return render_template('index.html') # 1
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
